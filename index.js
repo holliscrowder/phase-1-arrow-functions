1e5924525fb117335ffa43b0383dbe89c56ec20f
Hollis Crowder <hollis.crowder@gmail.com>
2024-03-11 15:28:48 -0700

    complete phase 1 arrow functinos lab

---
+++ index.js
@@ -0,0 +1,8 @@
+// divide (arrow)
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+
+// square (arrow)
+const square = (parameter) => parameter * parameter;
+
+// add (arrow)
+const add = (parameter1, parameter2) => parameter1 + parameter2;
